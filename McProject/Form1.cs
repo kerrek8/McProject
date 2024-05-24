a5ca696bc9bc3622687d947e026d5577e612f452
kerrek8 <kerrek8@gmail.com>
2024-05-24 10:01:01 +0500

    4 buttons done

--- McProject/Form1.cs
+++ McProject/Form1.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
+using System.Globalization;
 
 namespace McProject
 {
@@ -70,8 +71,6 @@ namespace McProject
                     zeros.Add(d[(int)fields.Item]);
                 }
             }
-
-
             //TODO: надо сделать красивый вывод на экран
             MessageBox.Show(zeros[0]);
         }
@@ -96,12 +95,40 @@ namespace McProject
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            List<string[]> sugars_calories = new List<string[]>();
+            foreach (string[] d in data) {
+                int sugars = int.Parse(d[(int)fields.Sugars]);
+                int calories = int.Parse(d[(int)fields.Calories]);
+                if ( sugars >= 1 && sugars <= 10) {
+                    if (calories >= 15 && calories <= 220) {
+                        string category = d[(int)fields.Category];
+                        string item = d[(int)fields.Item];
+                        string cal = d[(int)fields.Calories];
+                        string sug = d[(int)fields.Sugars];
+                        string size = d[(int)fields.ServingSize];
+                        string[] add_fields = new string[] {category,item,cal,sug,size }; 
+                        sugars_calories.Add(add_fields);
+                    }
+                }
+            }
+            MessageBox.Show(sugars_calories[0][1]);
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-
+            List<string[]> trans_items = new List<string[]>();
+            foreach (string[] d in data) {
+                double trans = double.Parse(d[(int)fields.TransFat], CultureInfo.InvariantCulture);
+                if (trans > 0) {
+                    string category = d[(int)fields.Category];
+                    string item = d[(int)fields.Item];
+                    string tr = d[(int)fields.TransFat];
+                    string size = d[(int)fields.ServingSize];
+                    string[] add_fields = new string[] {category,item,tr,size };
+                    trans_items.Add(add_fields);
+                }
+            }
+            MessageBox.Show(trans_items[0][1]);
         }
 
         private void button5_Click(object sender, EventArgs e)
